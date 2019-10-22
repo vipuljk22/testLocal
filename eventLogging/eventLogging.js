11faede1295b1c0f9f121a3c347f117f1bb03c72
Vipul Jha <vipuljha47@gmail.com>
2019-10-22 20:05:40 +0530

    second commit

--- eventLogging/eventLogging.js
+++ eventLogging/eventLogging.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     let eventJson = JSON.stringify(event);
-    console.log("Start : Logging event log");
+    console.log("Start : Logging event log24");
     console.log(eventJson);
     return {
         statusCode: 200,
